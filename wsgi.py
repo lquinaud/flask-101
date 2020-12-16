fae8aaadbb8c55e79b41e3fb0f415b9dba28eeed
laurent quinaud <lquinaud@gmail.com>
2020-12-16 10:36:32 +0100

    My first flask api

--- wsgi.py
+++ wsgi.py
@@ -2,9 +2,21 @@
 # pylint: disable=missing-docstring
 
 from flask import Flask
+from flask import jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+@app.route('/api/v1/products')
+def products():
+#    return "products api"
+	return jsonify(name=("Skello","Socialive.tv"), id=(1,2))
+
+#    PRODUCTS = {
+#      1: { 'id': 1, 'name': 'Skello' },
+#      2: { 'id': 2, 'name': 'Socialive.tv' },
+#    }
 
