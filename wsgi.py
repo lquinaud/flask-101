98438b09b24d1387ffb12025a693743ecbc8796c
laurent quinaud <lquinaud@gmail.com>
2020-12-18 15:36:48 +0100

    add products

--- wsgi.py
+++ wsgi.py
@@ -16,6 +16,7 @@ def products():
       1: { 'id': 1, 'name': 'Skello' },
       2: { 'id': 2, 'name': 'Socialive.tv' },
       3: { 'id': 3, 'name': 'Le Wagon'},
+      4: { 'id': 4, 'name': 'Lorenzo'},
     }
     #return jsonify(name=("Skello","Socialive.tv"), id=(1,2))
     return jsonify(PRODUCTS)
